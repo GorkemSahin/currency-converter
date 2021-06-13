49f96076e6984c44d31c8587d7746b0fb697acd1
Görkem Şahin <gorkem.sahin@cleevio.com>
2021-06-14 01:27:17 +0300

    Fix paths

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, '../app/build')))
 app.use(express.static('public'))
 
 app.use('/conversion', conversionRoutes)
--- server/src/common/config/env.config.ts
+++ server/src/common/config/env.config.ts
@@ -1,7 +1,7 @@
 export const config = {
   port: 3030,
-  appEndpoint: 'http://localhost:3000',
-  apiEndpoint: 'http://localhost:3030',
+  appEndpoint: 'http://localhost',
+  apiEndpoint: 'http://localhost',
   dbConnection:
     'mongodb+srv://user:pa55@purple-cluster.q0tvf.mongodb.net/assignment?retryWrites=true&w=majority',
   baseUrl: 'http://api.exchangeratesapi.io/v1',
